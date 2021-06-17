5fb3779efc8fd0ee88d6f8c1fc38ebc528c457f6
Cypress Kouri <72818091+CypressKouri@users.noreply.github.com>
2021-06-16 18:33:48 -0700

    Add files via upload

---
+++ Cypress-FactFinder/script.js
@@ -0,0 +1,15 @@
+function fact1(){
+	document.getElementById("b").innerHTML = "The first person convicted of speeding was going eight mph.<br><br><img src='images/car.jpeg' alt='car speeding on the highway'>";
+}
+function fact2(){
+	document.getElementById("b").innerHTML = "You lose up to 30 percent of your taste buds during flight.<br><br><img src='images/plane.jpeg' alt='plane window'>";
+}
+function fact3(){
+	document.getElementById("b").innerHTML = "The 100 folds in a chef's hat represent 100 ways to cook an egg.<br><br><img src='images/egg.jpg' alt='egg being prepared'>";
+}
+function fact4(){
+	document.getElementById("b").innerHTML = "The unicorn is the national animal of Scotland.<br><br><img src='images/scotland.jpeg' alt='train in scotland'>";
+}
+function fact5(){
+	document.getElementById("b").innerHTML = "You can major in wine at Cornell University.<br><br><img src='images/wine.jpg' alt='wine being poured'>";
+}
